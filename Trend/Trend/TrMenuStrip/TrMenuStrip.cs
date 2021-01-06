4ffbd166343ec95f2d1ee015d96f2160cd6b9e13
최선웅, Choi Seon Woong <escaco95@naver.com>
2021-01-06 20:25:38 +0900

    Hello World

---
+++ Trend/Trend/TrMenuStrip/TrMenuStrip.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Forms;
+
+namespace Trend
+{
+    /// <summary>
+    /// A trendy <seealso cref="MenuStrip"/> control.
+    /// </summary>
+    public class TrMenuStrip : MenuStrip
+    {
+
+    }
+}
